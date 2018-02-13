262d40568f10280664ef822741f461d795a9eb45
Bundar <birnie.nick@gmail.com>
2018-02-12 21:00:35 -0500

    commit: mymalloc

--- mymalloc.c
+++ mymalloc.c
@@ -6,7 +6,15 @@ Assignment 1: Build A Better Malloc
 */
 #include <stdio.h>
 #include "mymalloc.h"
-//mymalloc function. Returns pointer to memory allocated.
+/*
+Function: mymalloc
+-------------------------------------
+computes address of allocatable space in the array mem of 5000 bytes by treating it as a linked list of MemNodes which store basic meta data.
+size: ammount of space requested to be dynamically allocated.
+file: file name of file being ran.
+line: line number of current function call.
+returns: The pointer to the head of the memory with size equal to or greater than the ammount requested.
+*/
 void* mymalloc(size_t size, char* file, int line)
 {
   //treats the first addess of mem as if it was a MemNode.
@@ -27,5 +35,34 @@ void* mymalloc(size_t size, char* file, int line)
   p->active = *((unsigned short *)&p);
   p->next = &p + sizeof(MemNode) + size;
   //returns pointer to memory allocated right after meta data.
-  return p+sizeof(MemNode);
+  return (void*)(&p+sizeof(MemNode));
+}
+/*
+Function: myfree
+-------------------------------------
+Marks the memory that the pointer was using as addressable so it can be allocated again through mymalloc()
+ptr: pointer to memory that should be returned to useable memory.
+file: file name of file being ran.
+line: line number of current function call.
+returns: The pointer to the head of the memory with size equal to or greater than the ammount requested.
+*/
+void* free(void* ptr, char* file, int line)
+{
+  //checks if ptr points to a place in the array of 5000 chars
+  if(&ptr-sizeof(MemNode) < mem || &ptr-sizeof(MemNode) > &mem+5000)
+  {
+    printf("%s:%d error: out of bounds of addressable memory.\n",file,line);
+    return NULL;
+  }
+  MemNode node = &ptr-sizeof(MemNode);
+  //checks if ptr points to a valid previously allocated memory address
+  if(node->active != *((unsigned short *)&node))
+  {
+    printf("%s:%d error: pointer does not point to valid allocated memory address.\n",file,line);
+    return NULL;
+  }
+  else
+  {
+    
+  }
 }
